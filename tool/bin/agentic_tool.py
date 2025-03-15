82b598b60350b50e39235dfc51cc233c15d6abe7
陈虎明 <chenhuming@gmail.com>
2025-03-15 11:05:16 +0800

    Use streamlit to deploy agentic_tool.py

--- tool/bin/agentic_tool.py
+++ tool/bin/agentic_tool.py
@@ -6,9 +6,39 @@ from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from typing import List
 from typing_extensions import TypedDict
-import os, subprocess
+import os, subprocess, sys
+import streamlit as st
 
 
+# react_agent @see https://langchain-ai.github.io/langgraph/reference/prebuilt/#langgraph.prebuilt.chat_agent_executor.create_react_agent
+#
+# Task -> Agent -> Tool_Calls -> Tool -> Tool_Result -> Done
+#           ^                                |
+#           |--------------------------------+
+#
+# Execution:
+# 1.The agent node calls the LLM with the messages list (after applying the messages modifier).
+# 2.If the resulting AIMessage contains tool_calls, the graph will then call the tools.
+# 3.The tools node executes the tools (1 tool per tool_call) and adds the responses to the messages list as ToolMessage objects.
+# 4.The agent node then calls the LLM again.
+# 5.The process repeats until no more tool_calls are present in the response.
+# 6.The agent then returns the full list of messages as a dictionary containing the key "messages".
+class AgentState(TypedDict):
+    messages: List[BaseMessage]
+
+def format_for_model(state: AgentState):
+    return prompt.invoke({"messages": state["messages"]})
+
+
+# redirect all output to streamlit
+class StdOutRedirector:
+    def write(self, msg):
+        st.write(msg)
+
+sys.stdout = StdOutRedirector()
+
+
+# LLM settings
 llm = ChatOllama(model="qwen2.5", temperature=0.25)
 # llm = ChatOpenAI(api_key="",
 #                  model="deepseek-chat",
@@ -34,84 +64,84 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# react_agent @see https://langchain-ai.github.io/langgraph/reference/prebuilt/#langgraph.prebuilt.chat_agent_executor.create_react_agent
-#
-# Task -> Agent -> Tool_Calls -> Tool -> Tool_Result -> Done
-#           ^                                |
-#           |--------------------------------+
-#
-# Execution:
-# 1.The agent node calls the LLM with the messages list (after applying the messages modifier).
-# 2.If the resulting AIMessage contains tool_calls, the graph will then call the tools.
-# 3.The tools node executes the tools (1 tool per tool_call) and adds the responses to the messages list as ToolMessage objects.
-# 4.The agent node then calls the LLM again.
-# 5.The process repeats until no more tool_calls are present in the response.
-# 6.The agent then returns the full list of messages as a dictionary containing the key "messages".
 
-class AgentState(TypedDict):
-    messages: List[BaseMessage]
-
-def format_for_model(state: AgentState):
-    return prompt.invoke({"messages": state["messages"]})
-
+# tools definitions
+# @note remember to return either a result or a message to inform the LLM
 @tool
 def create_file(path: str, filename: str, content: str):
-    "Create a new file in a path with the specified content."
+    """Create a new file in a path with the specified content.
+
+    Args:
+        path:     The directory where to create the file
+        filename: The name of the file to create
+        content:  The content to write to the file
+    """
     try:
         expanded_path = os.path.expanduser(path + "/" + filename)
         with open(expanded_path, 'w') as file:
             file.write(content)
             return f"Created file {filename} in {path}"
     except Exception as e:
-        return str(e)
+        return f"Failed to create file {filename} in {path}: {e}"
 
 @tool
 def make_directory(name: str, parent: str):
-    "Create a new directory with the given name in the specified parent directory."
+    """Create a new directory with the given name in the specified parent directory.
+
+    Args:
+        name:   The name of the new directory to create, e.g. testdir
+        parent: The parent directory where the new directory should be created, e.g. /tmp
+    """
     try:
         dir_path = parent + "/" +name
         expanded_dir_path = os.path.expanduser(dir_path)
         os.makedirs(expanded_dir_path, exist_ok=True)
-        return f"Directory name created/verified in parent"
+        if os.path.exists(expanded_dir_path):
+            return f"Directory {name} created/verified in {parent}"
+        else:
+            return f"Failed to create directory {name} in {parent}"
     except Exception as e:
-        return str(e)
+        return f"Failed to create directory {name} in {parent}: {e}"
 
 # super-powerful, capable of replacing numerous existing tools.
 @tool
 def run_command(command: str):
-    "Run a command."
+    """Run a command.
+
+    Args:
+        command: Command to run
+    """
     try:
-        return subprocess.run(command, shell=True, capture_output=True, text=True).stdout
-    except Exception as e:
-        return str(e)
+        result = subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.stdout.decode()
+    except subprocess.CalledProcessError as e:
+        return f"Failed to run {command}: {e.stderr.decode()}"
 
 @tool
 def run_script(script_program: str, script_file: str, script_args: str):
-    "Run the script along with its specified arguments using the program."
+    """Run the script along with its specified arguments using the program.
+
+    Args:
+        script_program: Program to run the the script
+        script_file:    Path to the script to run.  Supports relative paths and ~
+        script_args:    Args for script to run
+    """
     try:
-        return subprocess.run([script_program, script_file,script_args], capture_output=True, text=True).stdout
-    except Exception as e:
-        return str(e)
+        result = subprocess.run([script_program, script_file,script_args],
+                                check=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        return f"Failed to run {script_file}: {e.stderre.decode()}"
 
-task1= """
-1.create a new directory '~/workspace/ai/testing'
-2.create 3 text files in this directory, each file with a poetry with at least 100 words.
-"""
 
-task2 = """
-1.create ~/workspace/ai/tool_script.py, it has following functions: it will print current time-stamp
-2.run the script ~/workspace/ai/tool_script.py
-"""
-
-task3= """
-1.Create a minesweeper game using html,css,js in directory '~/workspace/ai/minesweeper'
-2.run the game with http server
-"""
-
-def main():
+# enter point
+def run_agent(user_input: str):
     tools = [create_file, make_directory, run_command, run_script]
     agent = create_react_agent(llm, tools, state_modifier=format_for_model)
-    inputs = {"messages": [("user", task_prompt + task1)]}
+    inputs = {"messages": [("user", task_prompt + user_input)]}
 
     # increase recursion_limit to avoid agent stops prematurely
     for s in agent.stream(inputs, stream_mode="values", config={"recursion_limit": 100}):
@@ -120,6 +150,12 @@ def main():
             print(message)
         else:
             message.pretty_print()
+
+
+def main():
+    user_input = st.text_area("Please input your task for Agentic_Tool:")
+    if st.button("Submit") and user_input:
+        run_agent(user_input)
 
 
 if __name__ == "__main__":
