a46a10225636d456c3fd51057a20123a03e4e82e
陈虎明 <chenhuming@gmail.com>
2025-03-21 21:27:49 +0800

    Update docker_monitor.py

--- tool/bin/docker_monitor.py
+++ tool/bin/docker_monitor.py
@@ -2,6 +2,11 @@ import subprocess
 import time
 
 
+"""
+This script keeps monitoring containers, and start killing longest running container every 5 minutes.
+"""
+
+
 MAX_CONTAINER_LIMIT = 10
 
 def get_container_uptime():
