f29bcf0513f5b99c724185d05d9271ee7610f17d
陈虎明 <chenhuming@gmail.com>
2025-04-10 00:36:35 +0800

    Update inference_with_unsloth.py

--- finetune/inference_with_unsloth.py
+++ finetune/inference_with_unsloth.py
@@ -1,17 +1,21 @@
 from unsloth import FastLanguageModel
+from transformers import TextStreamer
 
+
+#1 Unsloth configuration
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False. # Choose any! We auto support RoPE Scaling internally!
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "qwen2.5-3B-finetuned", # YOUR MODEL YOU USED FOR TRAINING
+    model_name = "qwen2.5-3B-chat", # YOUR MODEL YOU USED FOR TRAINING
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
 )
 FastLanguageModel.for_inference(model) # Enable native 2x faster inference
 
+#2 Inference
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
@@ -23,15 +27,26 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 ### Response:
 {}"""
 
-inputs = tokenizer(
-[
-    alpaca_prompt.format(
-        "What is a famous tall tower in Paris?", # instruction
-        "", # input
-        "", # output - leave this blank for generation!
-    )
-], return_tensors = "pt").to("cuda")
+def run_inference():
+    while True:
+        user_input = input("Enter something: ")
 
-from transformers import TextStreamer
-text_streamer = TextStreamer(tokenizer)
-_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
+        if user_input == "quit":
+            print("bye byte !!!")
+            break;
+
+        inputs = tokenizer(
+        [
+            alpaca_prompt.format(
+                f"{user_input}", # instruction
+                "", # input
+                "", # output - leave this blank for generation!
+            )
+        ], return_tensors = "pt").to("cuda")
+
+        text_streamer = TextStreamer(tokenizer)
+        _ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
+
+
+if __name__ == "__main__":
+    run_inference()
