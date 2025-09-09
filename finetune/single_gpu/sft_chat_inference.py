1ff721c7fb07312c2214012ab3aa4d358f2c1af9
陈虎明 <chenhuming@gmail.com>
2025-09-09 16:59:28 +0900

    Add merge LoRA adapters.

--- finetune/single_gpu/sft_chat_inference.py
+++ finetune/single_gpu/sft_chat_inference.py
@@ -2,6 +2,7 @@ from unsloth import FastLanguageModel
 from transformers import TextStreamer
 from typing import Tuple
 import logging
+from peft import PeftModel
 
 
 # Configuration for Unsloth model
@@ -9,13 +10,15 @@ MAX_SEQ_LENGTH = 2048  # Auto supports RoPE Scaling
 DTYPE = None  # Auto-detects dtype (Float16 for Tesla T4/V100, Bfloat16 for Ampere+)
 LOAD_IN_4BIT = True  # Reduces memory usage with 4bit quantization
 
-# Initialize model and tokenizer
+# Initialize base model and tokenizer
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "qwen2.5-3B-chat", # YOUR MODEL YOU USED FOR TRAINING
+    model_name = "unsloth/Qwen2.5-3B-Instruct",  # Requires ~3GB VRAM
     max_seq_length = MAX_SEQ_LENGTH,
     dtype = DTYPE,
     load_in_4bit = LOAD_IN_4BIT,
 )
+# Merge LoRA adapters
+model = PeftModel.from_pretrained(model, "qwen2.5-3B-chat").merge_and_unload()
 FastLanguageModel.for_inference(model)  # Enable faster inference
 
 alpaca_prompt = """You are a naughty girlfriend, your task is to answer boyfriend's questions.
