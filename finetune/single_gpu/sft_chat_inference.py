63a952d7f6613cfab2ff150de746d68fb55dac12
陈虎明 <chenhuming@gmail.com>
2025-09-09 17:34:25 +0900

    Add some comments.

--- finetune/single_gpu/sft_chat_inference.py
+++ finetune/single_gpu/sft_chat_inference.py
@@ -2,7 +2,6 @@ from unsloth import FastLanguageModel
 from transformers import TextStreamer
 from typing import Tuple
 import logging
-from peft import PeftModel
 
 
 # Configuration for Unsloth model
@@ -10,15 +9,14 @@ MAX_SEQ_LENGTH = 2048  # Auto supports RoPE Scaling
 DTYPE = None  # Auto-detects dtype (Float16 for Tesla T4/V100, Bfloat16 for Ampere+)
 LOAD_IN_4BIT = True  # Reduces memory usage with 4bit quantization
 
-# Initialize base model and tokenizer
+# Initialize model and tokenizer, FastLanguageModel handle PEFT merge automatically
+# @see https://github.com/unslothai/unsloth/blob/a5968f30b2655dac89cee670d7db9a18bcb9728b/unsloth/models/loader.py
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Qwen2.5-3B-Instruct",  # Requires ~3GB VRAM
+    model_name = "qwen2.5-3B-chat", 
     max_seq_length = MAX_SEQ_LENGTH,
     dtype = DTYPE,
     load_in_4bit = LOAD_IN_4BIT,
 )
-# Merge LoRA adapters
-model = PeftModel.from_pretrained(model, "qwen2.5-3B-chat").merge_and_unload()
 FastLanguageModel.for_inference(model)  # Enable faster inference
 
 alpaca_prompt = """You are a naughty girlfriend, your task is to answer boyfriend's questions.
